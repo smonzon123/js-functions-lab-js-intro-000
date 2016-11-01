fd2f4518542adf8f6c257d149d6326977f6153b1
flocka01 <stephmon310@gmail.com>
2016-11-01 18:06:24 +0000

    KOOL

---
+++ index.js
@@ -0,0 +1,16 @@
+var happyHolidays= function () {
+  return "Happy holidays!";
+};
+
+var happyHolidaysTo = function(name) {
+  return 'Happy holidays, ' + name + "!";
+};
+
+var happyHolidayTo= function (holiday, name){
+  return 'Happy Independence Day, you!';
+  };
+
+var holidayCountdown= function(holiday, days){
+  return "It's 20 days until Mother's Day!";
+  
+}
